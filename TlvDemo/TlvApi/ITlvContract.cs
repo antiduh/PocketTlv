c59160b80f0798d2439bac50ba952e8e4c6ef573
antiduh <antiduh@gmail.com>
2020-08-21 05:02:14 +0000

    Comments.

--- TlvDemo/TlvApi/ITlvContract.cs
+++ TlvDemo/TlvApi/ITlvContract.cs
@@ -2,12 +2,28 @@
 
 namespace TlvDemo.TlvApi
 {
+    /// <summary>
+    /// Specifies the interface for complete messages composed of various TLV parts.
+    /// </summary>
     public interface ITlvContract
     {
+        /// <summary>
+        /// Gets a value that uniquely identifies this contract from all other contracts. The
+        /// ContractID is written to the byte stream in order to reconstitute the contract during
+        /// the deserialization process.
+        /// </summary>
         int ContractId { get; }
 
+        /// <summary>
+        /// Reads the contract's values from TLV tags.
+        /// </summary>
+        /// <param name="parseContext"></param>
         void Parse( ITlvParseContext parseContext );
 
+        /// <summary>
+        /// Writes the contract's values as TLV tags.
+        /// </summary>
+        /// <param name="saveContext"></param>
         void Save( ITlvSaveContext saveContext );
     }
 }
