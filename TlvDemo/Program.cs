28aa04527318b1f83c3d5a6359923b7aa4cf08d3
antiduh <antiduh@gmail.com>
2020-08-21 03:08:59 +0000

    Removed dead code.

--- TlvDemo/Program.cs
+++ TlvDemo/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using TlvDemo.Messages;
 using TlvDemo.TlvApi;
 
 namespace TlvDemo
@@ -79,54 +78,6 @@ namespace TlvDemo
 
             TlvReader reader = new TlvReader( stream );
             return reader.Read<T>();
-        }
-
-#if false
-        private static void LoadStoreTest()
-        {
-            var top = new CompositeTag( 1,
-                new StringTag( 2, "Hello" ),
-                new IntTag( 3, 42 ),
-                new CompositeTag( 4, 
-                    new CompositeTag( 5,
-                        new IntTag( 6, 111 ),
-                        new IntTag( 7, 112 ),
-                        new IntTag( 8, 113 )
-                    )
-                ),
-                new IntTag( 9, 1011 )
-            );
-            
-
-            var stream = new MemoryStream();
-            TlvWriter writer = new TlvWriter( stream );
-
-            writer.Write( top );
-
-            byte[] buffer = stream.GetBuffer();
-
-            // -- Read --
-
-            ITag rebuiltTop;
-
-            stream.Position = 0L;
-            var reader = new TlvReader( stream );
-
-            rebuiltTop = reader.Read();
-        }
-
-#endif
-
-        private static void ConstructTest()
-        {
-            var frame = new ClientFrame()
-            {
-                Exchange = "voren-core",
-                RoutingKey = "voren.Echo",
-                MessageName = "EchoRequest",
-                Guid = "123456",
-                //    Message = new EchoRequest() { Text = "Hello world." }
-            };
         }
     }
 }
