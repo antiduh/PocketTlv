fc82216a932c622f735a3d9a28f888cccb2df370
antiduh <antiduh@gmail.com>
2020-08-21 04:04:36 +0000

    Removed unused code now that the demo cases have been laid out correctly.

--- TlvDemo/Program.cs
+++ TlvDemo/Program.cs
@@ -43,7 +43,7 @@ namespace TlvDemo
         }
 
         /// <summary>
-        /// Demonstrates how to write out a simple contract that has statically resolved members.
+        /// Demonstrates how to write out a simple contract that has statically defined and resolved members.
         /// </summary>
         private static void SimpleContractTest()
         {
@@ -155,58 +155,6 @@ namespace TlvDemo
 
             var personRecordCopy = secondCopy.Message.Resolve<PersonRecord>();
             Debug.Assert( personRecordCopy.Equals( personRecord ) );
-        }
-
-        private static void LoadStorePersonRecord()
-        {
-            // --- Contracts can be statically composed ---
-            PersonRecord personRecordOrig = new PersonRecord()
-            {
-                Name = "Kevin Thompson",
-                Age = 37,
-                Address = new AddressRecord() { LotNumber = 50, StreetName = "50 Hampden Rd" },
-            };
-
-            // --- Contracts can by dynamically composed ---
-            MessageRecord messageRecord = new MessageRecord()
-            {
-                Name = "VorenEcho",
-                Message = personRecordOrig
-            };
-
-            var messageCopy = RoundTrip( messageRecord );
-
-            PersonRecord personCopy;
-
-            // Method 1:
-
-            personCopy = messageCopy.Message.Resolve<PersonRecord>();
-            ProcessPersonRecord( personCopy );
-
-            // Method 2:
-            if( messageCopy.Message.ContractId == PersonRecord.ContractId )
-            {
-                personCopy = messageCopy.Message.Resolve<PersonRecord>();
-                ProcessPersonRecord( personCopy );
-            }
-
-            // Method 3:
-            if( messageCopy.Message.TryResolve<PersonRecord>( out personCopy ) )
-            {
-                ProcessPersonRecord( personCopy );
-            }
-
-            // --- Incomplete constructions can be relayed in their incomplete form ---
-            messageCopy = RoundTrip( messageRecord );
-            var messageCopy2 = RoundTrip( messageCopy );
-
-            personCopy = messageCopy2.Message.Resolve<PersonRecord>();
-            ProcessPersonRecord( personCopy );
-        }
-
-        private static void ProcessPersonRecord( PersonRecord record )
-        {
-            Console.WriteLine( record.Name );
         }
 
         private static ITag RoundTrip( ITag tag )
