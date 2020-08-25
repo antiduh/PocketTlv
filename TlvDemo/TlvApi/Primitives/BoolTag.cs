69acad07fe8054777286f99b0a527f5e547bfd71
antiduh <antiduh@gmail.com>
2020-08-25 06:56:38 +0000

    Implemented equality, hashing, and implicit conversion for the BoolTag type.

--- TlvDemo/TlvApi/Primitives/BoolTag.cs
+++ TlvDemo/TlvApi/Primitives/BoolTag.cs
@@ -27,6 +27,40 @@ namespace TlvDemo.TlvApi.Primitives
 
         public int FieldId { get; set; }
 
+        public override bool Equals( object other )
+        {
+            return Equals( other as BoolTag );
+        }
+
+        public bool Equals( BoolTag other )
+        {
+            if( ReferenceEquals( other, null ) )
+            {
+                return false;
+            }
+            else if( ReferenceEquals( other, this ) )
+            {
+                return true;
+            }
+            else
+            {
+                return this.Value == other.Value;
+            }
+        }
+
+
+        public override int GetHashCode()
+        {
+            return this.Value.GetHashCode();
+        }
+
+        public static implicit operator bool( BoolTag tag )
+        {
+            return tag.Value;
+        }
+
+        // --- ITag implementation ---
+
         WireType ITag.WireType => WireType.Bool;
 
         int ITag.ComputeLength()
