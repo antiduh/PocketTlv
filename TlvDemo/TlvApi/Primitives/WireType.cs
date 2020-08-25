9586fa47c132028c7825cae5f79718c95ea4ce63
antiduh <antiduh@gmail.com>
2020-08-25 06:44:53 +0000

    Rearranged the order of the WireType elements to fit some sort of logic. Still arbitrary.

--- TlvDemo/TlvApi/Primitives/WireType.cs
+++ TlvDemo/TlvApi/Primitives/WireType.cs
@@ -6,13 +6,13 @@ namespace TlvDemo.TlvApi
     {
         Composite = 1,
         ContractId = 2,
-        Int = 3,
+        Bool = 3,
         String = 4,
-        Double = 5,
-        ByteArray = 6,
+        Short = 5, 
+        Int = 6,
         Long = 7,
-        Short = 8,
-        VarInt = 9,
-        Bool = 10,
+        VarInt = 8,
+        Double = 9,
+        ByteArray = 10,
     }
 }
