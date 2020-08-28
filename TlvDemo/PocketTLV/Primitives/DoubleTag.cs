33e0b1b424684f677cae603c8e0f9f3af61d7db5
antiduh <antiduh@gmail.com>
2020-08-28 18:10:34 +0000

    Added comments.

--- TlvDemo/PocketTLV/Primitives/DoubleTag.cs
+++ TlvDemo/PocketTLV/Primitives/DoubleTag.cs
@@ -8,21 +8,35 @@ namespace PocketTLV.Primitives
     /// </summary>
     public class DoubleTag : ITag
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DoubleTag"/> class with default values.
+        /// </summary>
         public DoubleTag()
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DoubleTag"/> class.
+        /// </summary>
+        /// <param name="value">The double value to store.</param>
         public DoubleTag( double value )
         {
             this.Value = value;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DoubleTag"/> class.
+        /// </summary>
+        /// <param name="value">The double value to store.</param>
         public DoubleTag( int fieldId, double value )
         {
             this.FieldId = fieldId;
             this.Value = value;
         }
 
+        /// <summary>
+        /// Gets or sets the double value stored by the tag.
+        /// </summary>
         public double Value { get; set; }
 
         /// <summary>
@@ -30,31 +44,59 @@ namespace PocketTLV.Primitives
         /// </summary>
         public int FieldId { get; set; }
 
+        /// <summary>
+        /// Converts the <see cref="DoubleTag>"/>'s value to a string.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return this.Value.ToString();
         }
 
+        /// <summary>
+        /// Returns whether this <see cref="DoubleTag"/> is equal to the provided object.
+        /// </summary>
+        /// <param name="other">The object to compare to.</param>
+        /// <returns>True if the other object has the same value as this object, false otherwise.</returns>
         public override bool Equals( object other )
         {
             return Equals( other as DoubleTag );
         }
 
+        /// <summary>
+        /// Compares two <see cref="DoubleTag"/> objects.
+        /// </summary>
+        /// <param name="other">The tag to compare to.</param>
+        /// <returns>True if the tag object has the same value as this tag, false otherwise.</returns>
         public bool Equals( DoubleTag other )
         {
             return this == other;
         }
 
+        /// <summary>
+        /// Returns a hash value for a <see cref="DoubleTag"/> instance.
+        /// </summary>
+        /// <returns>An integer hash code.</returns>
         public override int GetHashCode()
         {
             return this.Value.GetHashCode();
         }
 
+        /// <summary>
+        /// Converts a <see cref="DoubleTag"/> to a <see cref="double"/>.
+        /// </summary>
+        /// <param name="tag">The tag to convert.</param>
         public static implicit operator double( DoubleTag tag )
         {
             return tag.Value;
         }
 
+        /// <summary>
+        /// Compares two <see cref="DoubleTag"/> objects to determine if they contain the same value.
+        /// </summary>
+        /// <param name="left">The first tag to compare.</param>
+        /// <param name="right">The second tag to compare.</param>
+        /// <returns>True if the two tags contain equal values, false otherwise.</returns>
         public static bool operator ==( DoubleTag left, DoubleTag right )
         {
             if( left is null )
@@ -71,6 +113,12 @@ namespace PocketTLV.Primitives
             }
         }
 
+        /// <summary>
+        /// Compares two <see cref="DoubleTag"/> objects to determine if they contain different values.
+        /// </summary>
+        /// <param name="left">The first tag to compare.</param>
+        /// <param name="right">The second tag to compare.</param>
+        /// <returns>True if the two tags do not contain equal values, false otherwise.</returns>
         public static bool operator !=( DoubleTag left, DoubleTag right )
         {
             return !( left == right );
