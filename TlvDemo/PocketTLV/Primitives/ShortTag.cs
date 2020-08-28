9c6e7d75e4f6feac8231f13b37c58fea354da56f
antiduh <antiduh@gmail.com>
2020-08-28 18:25:29 +0000

    Comments.

--- TlvDemo/PocketTLV/Primitives/ShortTag.cs
+++ TlvDemo/PocketTLV/Primitives/ShortTag.cs
@@ -8,21 +8,35 @@ namespace PocketTLV.Primitives
     /// </summary>
     public class ShortTag : ITag
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShortTag"/> class with default values.
+        /// </summary>
         public ShortTag()
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShortTag"/> class.
+        /// </summary>
+        /// <param name="value">The short value to store.</param>
         public ShortTag( short value )
         {
             this.Value = value;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShortTag"/> class.
+        /// </summary>
+        /// <param name="value">The short value to store.</param>
         public ShortTag( int fieldId, short value )
         {
             this.FieldId = fieldId;
             this.Value = value;
         }
 
+        /// <summary>
+        /// Gets or sets the short value stored by the tag.
+        /// </summary>
         public short Value { get; set; }
 
         /// <summary>
@@ -30,31 +44,59 @@ namespace PocketTLV.Primitives
         /// </summary>
         public int FieldId { get; set; }
 
+        /// <summary>
+        /// Converts the <see cref="ShortTag>"/>'s value to a string.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return this.Value.ToString();
         }
 
+        /// <summary>
+        /// Returns whether this <see cref="ShortTag"/> is equal to the provided object.
+        /// </summary>
+        /// <param name="other">The object to compare to.</param>
+        /// <returns>True if the other object has the same value as this object, false otherwise.</returns>
         public override bool Equals( object other )
         {
             return Equals( other as ShortTag );
         }
 
+        /// <summary>
+        /// Compares two <see cref="ShortTag"/> objects.
+        /// </summary>
+        /// <param name="other">The tag to compare to.</param>
+        /// <returns>True if the tag object has the same value as this tag, false otherwise.</returns>
         public bool Equals( ShortTag other )
         {
             return this == other;
         }
 
+        /// <summary>
+        /// Returns a hash value for a <see cref="ShortTag"/> instance.
+        /// </summary>
+        /// <returns>An integer hash code.</returns>
         public override int GetHashCode()
         {
             return this.Value.GetHashCode();
         }
 
+        /// <summary>
+        /// Converts a <see cref="ShortTag"/> to a <see cref="short"/>.
+        /// </summary>
+        /// <param name="tag">The tag to convert.</param>
         public static implicit operator short( ShortTag tag )
         {
             return tag.Value;
         }
 
+        /// <summary>
+        /// Compares two <see cref="ShortTag"/> objects to determine if they contain the same value.
+        /// </summary>
+        /// <param name="left">The first tag to compare.</param>
+        /// <param name="right">The second tag to compare.</param>
+        /// <returns>True if the two tags contain equal values, false otherwise.</returns>
         public static bool operator ==( ShortTag left, ShortTag right )
         {
             if( left is null )
@@ -71,6 +113,12 @@ namespace PocketTLV.Primitives
             }
         }
 
+        /// <summary>
+        /// Compares two <see cref="ShortTag"/> objects to determine if they contain different values.
+        /// </summary>
+        /// <param name="left">The first tag to compare.</param>
+        /// <param name="right">The second tag to compare.</param>
+        /// <returns>True if the two tags do not contain equal values, false otherwise.</returns>
         public static bool operator !=( ShortTag left, ShortTag right )
         {
             return !( left == right );
