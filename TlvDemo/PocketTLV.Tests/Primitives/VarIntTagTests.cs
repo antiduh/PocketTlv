d42ff93b59b8b7c628874b53ce9818403f20a7c8
antiduh <antiduh@gmail.com>
2020-08-28 04:52:48 +0000

    Wrote a quick unit test for the VarIntTag.
    Fixed the broken serialization logic in VarIntTag.

---
+++ TlvDemo/PocketTLV.Tests/Primitives/VarIntTagTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PocketTLV.Primitives;
+using PocketTLV.Tests.Inftrastructure;
+
+namespace PocketTLV.Tests.Primitives
+{
+    [TestClass]
+    public class VarIntTagTests
+    {
+        [DataList( long.MinValue, int.MinValue, -2, -1, 0, 1, 2, int.MaxValue, long.MaxValue )]
+        [DataTestMethod]
+        public void When_SerializingRoundTrip_TagsAreEqual( long value )
+        {
+            const int maxLength = 8;
+            byte[] buffer = new byte[maxLength];
+
+            ITag sourceTag = new VarIntTag( value );
+            ITag destTag = new VarIntTag();
+
+            int length = sourceTag.ComputeLength();
+
+            sourceTag.WriteValue( buffer, 0 );
+            destTag.ReadValue( buffer, 0, length );
+
+            Assert.AreEqual( sourceTag, destTag );
+            Assert.AreEqual( value, ((VarIntTag)destTag).Value );
+        }
+    }
+}
--- TlvDemo/PocketTLV/Primitives/VarIntTag.cs
+++ TlvDemo/PocketTLV/Primitives/VarIntTag.cs
@@ -83,7 +83,7 @@ namespace PocketTLV.Primitives
         {
             int numBytes = 0;
 
-            for( long copy = this.Value; copy > 0; copy >>= 8 )
+            for( ulong copy = (ulong)this.Value; copy > 0; copy >>= 8 )
             {
                 numBytes++;
             }
@@ -93,18 +93,19 @@ namespace PocketTLV.Primitives
 
         void ITag.ReadValue( byte[] buffer, int position, int length )
         {
-            this.Value = 0;
+            ulong result = 0;
 
             for( int i = 0; i < length; i++ )
             {
-                this.Value <<= 8;
-                this.Value |= buffer[position + i];
+                result |= ( (ulong)buffer[position + i] ) << i * 8;
             }
+
+            this.Value = (long)result;
         }
 
         void ITag.WriteValue( byte[] buffer, int position )
         {
-            long copy = this.Value;
+            ulong copy = (ulong)this.Value;
 
             for( int i = 0; i < ComputeLength(); i++ )
             {
