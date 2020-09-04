41406aa5cf6569987d4f4795fcdbddfba40cb57d
antiduh <antiduh@gmail.com>
2020-09-04 19:55:03 +0000

    Comments.

--- TlvDemo/PocketTlv/TlvParseContext.cs
+++ TlvDemo/PocketTlv/TlvParseContext.cs
@@ -9,32 +9,108 @@ namespace PocketTlv
     /// </summary>
     public interface ITlvParseContext
     {
+        /// <summary>
+        /// Determines whether the TLV stream contains the specified field.
+        /// </summary>
+        /// <param name="fieldId">The ID of the field to locate.</param>
+        /// <returns></returns>
         bool HasField( int fieldId );
 
-        bool TryTag<T>( int fieldId, out T tag );
+        /// <summary>
+        /// Attempts to parse a <see cref="ITag"/> from the TLV stream. A return value indicates
+        /// whether the operation succeeded.
+        /// </summary>
+        /// <typeparam name="T">The <see cref="ITag"/> type to parse.</typeparam>
+        /// <param name="fieldId">The ID of the field to parse.</param>
+        /// <param name="tag">If successful, returns the parsed tag.</param>
+        /// <returns>True if the field was successfully located, false otherwise.</returns>
+        bool TryTag<T>( int fieldId, out T tag ) where T : ITag;
 
+        /// <summary>
+        /// Attempts to parse a <see cref="ITlvContract"/> of the given type from the TLV stream. A return value
+        /// indicates whether the operation succeeded.
+        /// </summary>
+        /// <typeparam name="T">The <see cref="ITlvContract"/> to parse.</typeparam>
+        /// <param name="fieldId">The ID of the field to parse.</param>
+        /// <param name="contract">If successful, returns the parsed contract.</param>
+        /// <returns>True if the field was successfully located, falase otherwise.</returns>
         bool TryContract<T>( int fieldId, out T contract ) where T : ITlvContract, new();
 
+        /// <summary>
+        /// Attempts to parse a <see cref="ITlvContract"/> from the TLV stream. A return value
+        /// indicates whether the operation succeeded.
+        /// </summary>
+        /// <param name="fieldId">The ID of the field to parse.</param>
+        /// <param name="contract">If successful, returns the parsed contract.</param>
+        /// <returns>True if the field was successfully located, falase otherwise.</returns>
         bool TryContract( int fieldId, out ITlvContract contract );
 
+        /// <summary>
+        /// Parses a <see cref="ITag"/> from the TLV stream.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="fieldId"></param>
+        /// <returns>The parsed tag.</returns>
+        /// <exception cref="KeyNotFoundException">
+        /// No field with the value <paramref name="fieldId"/> could be found.
+        /// </exception>
         T Tag<T>( int fieldId ) where T : ITag;
 
+        /// <summary>
+        /// Parses a <see cref="ITlvContract"/> of the given type from the TLV stream.
+        /// </summary>
+        /// <typeparam name="T">The <see cref="ITlvContract"/> type to parse.</typeparam>
+        /// <param name="fieldId">The ID of the field to parse.</param>
+        /// <returns>The parsed contract</returns>
+        /// <exception cref="KeyNotFoundException">
+        /// No field with the value <paramref name="fieldId"/> could be found.
+        /// </exception>
+        /// <exception cref="InvalidCastException">
+        /// The contract with the value <paramref name="fieldId"/> does not match the type
+        /// <typeparamref name="T"/>.
+        /// </exception>
         T Contract<T>( int fieldId ) where T : ITlvContract, new();
 
+        /// <summary>
+        /// Parses a <see cref="ITlvContract"/> from the TLV stream.
+        /// </summary>
+        /// <param name="fieldId">The ID of the field to parse.</param>
+        /// <returns>The parsed contract.</returns>
+        /// <exception cref="KeyNotFoundException">
+        /// No field with the value <paramref name="fieldId"/> could be found.
+        /// </exception>
         ITlvContract Contract( int fieldId );
     }
 
+    /// <summary>
+    /// Parses TLV fields from a <see cref="CompositeTag"/>.
+    /// </summary>
     public class TlvParseContext : ITlvParseContext
     {
         private CompositeTag source;
         private readonly bool hideFirst;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TlvParseContext"/> class.
+        /// </summary>
+        /// <param name="source">
+        /// The <see cref="CompositeTag"/> that contains the TLV stream for the contract.
+        /// </param>
+        /// <param name="hideFirst">
+        /// If true, the first TLV tag in the source <see cref="CompositeTag"/> is hidden from view
+        /// of callers.
+        /// </param>
         public TlvParseContext( CompositeTag source, bool hideFirst )
         {
             this.source = source;
             this.hideFirst = hideFirst;
         }
 
+        /// <summary>
+        /// Determines whether the TLV stream contains the specified field.
+        /// </summary>
+        /// <param name="fieldId">The ID of the field to locate.</param>
+        /// <returns></returns>
         public bool HasField( int fieldId )
         {
             var children = source.Children;
@@ -50,7 +126,15 @@ namespace PocketTlv
             return false;
         }
 
-        public bool TryTag<T>( int fieldId, out T tag )
+        /// <summary>
+        /// Attempts to parse a <see cref="ITag"/> from the TLV stream. A return value indicates
+        /// whether the operation succeeded.
+        /// </summary>
+        /// <typeparam name="T">The <see cref="ITag"/> type to parse.</typeparam>
+        /// <param name="fieldId">The ID of the field to parse.</param>
+        /// <param name="tag">If successful, returns the parsed tag.</param>
+        /// <returns>True if the field was successfully located, false otherwise.</returns>
+        public bool TryTag<T>( int fieldId, out T tag ) where T : ITag 
         {
             var children = source.Children;
 
@@ -67,6 +151,14 @@ namespace PocketTlv
             return true;
         }
 
+        /// <summary>
+        /// Attempts to parse a <see cref="ITlvContract"/> of the given type from the TLV stream. A return value
+        /// indicates whether the operation succeeded.
+        /// </summary>
+        /// <typeparam name="T">The <see cref="ITlvContract"/> to parse.</typeparam>
+        /// <param name="fieldId">The ID of the field to parse.</param>
+        /// <param name="contract">If successful, returns the parsed contract.</param>
+        /// <returns>True if the field was successfully located, falase otherwise.</returns>
         public bool TryContract<T>( int fieldId, out T contract ) where T : ITlvContract, new()
         {
             // Technically this method could be implemented by calling `TryContract( int,
@@ -95,6 +187,13 @@ namespace PocketTlv
             return true;
         }
 
+        /// <summary>
+        /// Attempts to parse a <see cref="ITlvContract"/> from the TLV stream. A return value
+        /// indicates whether the operation succeeded.
+        /// </summary>
+        /// <param name="fieldId">The ID of the field to parse.</param>
+        /// <param name="contract">If successful, returns the parsed contract.</param>
+        /// <returns>True if the field was successfully located, falase otherwise.</returns>
         public bool TryContract( int fieldId, out ITlvContract contract )
         {
             CompositeTag contractTag;
@@ -110,6 +209,15 @@ namespace PocketTlv
             return false;
         }
 
+        /// <summary>
+        /// Parses a <see cref="ITag"/> from the TLV stream.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="fieldId"></param>
+        /// <returns>The parsed tag.</returns>
+        /// <exception cref="KeyNotFoundException">
+        /// No field with the value <paramref name="fieldId"/> could be found.
+        /// </exception>
         public T Tag<T>( int fieldId ) where T : ITag
         {
             if( TryTag<T>( fieldId, out T tag ) )
@@ -120,6 +228,19 @@ namespace PocketTlv
             throw new KeyNotFoundException( $"No TLV value was found with fieldId = {fieldId}." );
         }
 
+        /// <summary>
+        /// Parses a <see cref="ITlvContract"/> of the given type from the TLV stream.
+        /// </summary>
+        /// <typeparam name="T">The <see cref="ITlvContract"/> type to parse.</typeparam>
+        /// <param name="fieldId">The ID of the field to parse.</param>
+        /// <returns>The parsed contract</returns>
+        /// <exception cref="KeyNotFoundException">
+        /// No field with the value <paramref name="fieldId"/> could be found.
+        /// </exception>
+        /// <exception cref="InvalidCastException">
+        /// The contract with the value <paramref name="fieldId"/> does not match the type
+        /// <typeparamref name="T"/>.
+        /// </exception>
         public T Contract<T>( int fieldId ) where T : ITlvContract, new()
         {
             if( TryContract<T>( fieldId, out T contract ) )
@@ -130,6 +251,14 @@ namespace PocketTlv
             throw new KeyNotFoundException( $"No TLV value was found with fieldId = {fieldId}." );
         }
 
+        /// <summary>
+        /// Parses a <see cref="ITlvContract"/> from the TLV stream.
+        /// </summary>
+        /// <param name="fieldId">The ID of the field to parse.</param>
+        /// <returns>The parsed contract.</returns>
+        /// <exception cref="KeyNotFoundException">
+        /// No field with the value <paramref name="fieldId"/> could be found.
+        /// </exception>
         public ITlvContract Contract( int fieldId )
         {
             if( TryContract( fieldId, out ITlvContract contract ) )
@@ -140,13 +269,13 @@ namespace PocketTlv
             throw new KeyNotFoundException( $"No TLV value was found with fieldId = {fieldId}." );
         }
 
-        private bool GetContractSubTag( int fieldId, out CompositeTag contractTag, out int foundContractId )
+        private bool GetContractSubTag( int fieldId, out CompositeTag subContractTag, out int foundContractId )
         {
-            if( TryTag<CompositeTag>( fieldId, out contractTag ) )
+            if( TryTag<CompositeTag>( fieldId, out subContractTag ) )
             {
                 // See TlvSaveContext.Save. We use value-stuffing to save the contract ID of the
                 // serialized contract.
-                foundContractId = (ContractIdTag)contractTag.Children.First();
+                foundContractId = (ContractIdTag)subContractTag.Children.First();
                 return true;
             }
             else
