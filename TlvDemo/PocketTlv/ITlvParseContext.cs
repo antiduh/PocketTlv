7f06bc1f8745c1da9362050ce1284afd398a602e
antiduh <antiduh@gmail.com>
2020-09-09 02:01:38 +0000

    Split the save/parse contexts to file separate from their interfaces.

---
+++ TlvDemo/PocketTlv/ITlvParseContext.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PocketTlv
+{
+    /// <summary>
+    /// Specifies the interface for parsing TLV fields from a contract's TLV stream.
+    /// </summary>
+    public interface ITlvParseContext
+    {
+        /// <summary>
+        /// Determines whether the TLV stream contains the specified field.
+        /// </summary>
+        /// <param name="fieldId">The ID of the field to locate.</param>
+        /// <returns></returns>
+        bool HasField( int fieldId );
+
+        /// <summary>
+        /// Attempts to parse a <see cref="ITag"/> from the TLV stream. A return value indicates
+        /// whether the operation succeeded.
+        /// </summary>
+        /// <typeparam name="T">The <see cref="ITag"/> type to parse.</typeparam>
+        /// <param name="fieldId">The ID of the field to parse.</param>
+        /// <param name="tag">If successful, returns the parsed tag.</param>
+        /// <returns>True if the field was successfully located, false otherwise.</returns>
+        bool TryTag<T>( int fieldId, out T tag ) where T : ITag;
+
+        /// <summary>
+        /// Attempts to parse a <see cref="ITlvContract"/> of the given type from the TLV stream. A return value
+        /// indicates whether the operation succeeded.
+        /// </summary>
+        /// <typeparam name="T">The <see cref="ITlvContract"/> to parse.</typeparam>
+        /// <param name="fieldId">The ID of the field to parse.</param>
+        /// <param name="contract">If successful, returns the parsed contract.</param>
+        /// <returns>True if the field was successfully located, falase otherwise.</returns>
+        bool TryContract<T>( int fieldId, out T contract ) where T : ITlvContract, new();
+
+        /// <summary>
+        /// Attempts to parse a <see cref="ITlvContract"/> from the TLV stream. A return value
+        /// indicates whether the operation succeeded.
+        /// </summary>
+        /// <param name="fieldId">The ID of the field to parse.</param>
+        /// <param name="contract">If successful, returns the parsed contract.</param>
+        /// <returns>True if the field was successfully located, falase otherwise.</returns>
+        bool TryContract( int fieldId, out ITlvContract contract );
+
+        /// <summary>
+        /// Parses a <see cref="ITag"/> from the TLV stream.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="fieldId"></param>
+        /// <returns>The parsed tag.</returns>
+        /// <exception cref="KeyNotFoundException">
+        /// No field with the value <paramref name="fieldId"/> could be found.
+        /// </exception>
+        T Tag<T>( int fieldId ) where T : ITag;
+
+        /// <summary>
+        /// Parses a <see cref="ITlvContract"/> of the given type from the TLV stream.
+        /// </summary>
+        /// <typeparam name="T">The <see cref="ITlvContract"/> type to parse.</typeparam>
+        /// <param name="fieldId">The ID of the field to parse.</param>
+        /// <returns>The parsed contract</returns>
+        /// <exception cref="KeyNotFoundException">
+        /// No field with the value <paramref name="fieldId"/> could be found.
+        /// </exception>
+        /// <exception cref="InvalidCastException">
+        /// The contract with the value <paramref name="fieldId"/> does not match the type
+        /// <typeparamref name="T"/>.
+        /// </exception>
+        T Contract<T>( int fieldId ) where T : ITlvContract, new();
+
+        /// <summary>
+        /// Parses a <see cref="ITlvContract"/> from the TLV stream.
+        /// </summary>
+        /// <param name="fieldId">The ID of the field to parse.</param>
+        /// <returns>The parsed contract.</returns>
+        /// <exception cref="KeyNotFoundException">
+        /// No field with the value <paramref name="fieldId"/> could be found.
+        /// </exception>
+        ITlvContract Contract( int fieldId );
+    }
+}
--- TlvDemo/PocketTlv/ITlvSaveContext.cs
+++ TlvDemo/PocketTlv/ITlvSaveContext.cs
@@ -8,47 +8,4 @@ namespace PocketTlv
 
         void Contract( int fieldId, ITlvContract subContract );
     }
-
-    public class TlvSaveContext : ITlvSaveContext
-    {
-        private CompositeTag contractTag;
-
-        public TlvSaveContext( CompositeTag contractTag )
-        {
-            this.contractTag = contractTag;
-        }
-
-        public void Tag( int fieldId, ITag tag )
-        {
-            this.contractTag.AddChild( fieldId, tag );
-        }
-
-        public void Contract( int fieldId, ITlvContract subContract )
-        {
-            // When saving sub-contracts, we do "value-stuffing":
-            // - It's handy to have the contract ID when parsing, for error checking.
-            // - It's necessary to have the contract ID when doing deferred parsing for
-            //   UnknownContract resolution after the fact.
-            // - So we "value-stuff": we put in our own tag in before the contract's tags, and then
-            //   hide the value from the real type when they read from the CompositeTag.
-            // - The actual value of the fieldId we pass down doesn't matter. It never gets used.
-
-            // A tag to represent the subcontract.
-            var subcontractTag = new CompositeTag();
-
-            // Value-stuff the contract ID.
-            subcontractTag.AddChild( 0xABC, new ContractIdTag( subContract.ContractId ) );
-
-            // Tell the contract to serialize itself through us by swapping which CompositeTag we're
-            // pointing to (effectively doing recursion via the call stack).
-            CompositeTag backup = this.contractTag;
-
-            this.contractTag = subcontractTag;
-            subContract.Save( this );
-            this.contractTag = backup;
-
-            // Save the composite tag representing the subcontract to our parent.
-            this.contractTag.AddChild( fieldId, subcontractTag );
-        }
-    }
 }
