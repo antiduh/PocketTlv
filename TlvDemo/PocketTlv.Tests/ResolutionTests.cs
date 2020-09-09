d96de2067cdd3e02c7720cfb30dc4f6616f039ba
antiduh <antiduh@gmail.com>
2020-09-09 01:59:37 +0000

    Created a test to show what happens when a contract is anonymously transferred.

---
+++ TlvDemo/PocketTlv.Tests/ResolutionTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PocketTlv.Tests.Inftrastructure.StubContracts;
+
+namespace PocketTlv.Tests
+{
+    [TestClass]
+    public class ResolutionTests
+    {
+        [TestMethod]
+        public void EmbeddedUnresolvedContract_DoubleParse()
+        {
+            var carrier = new CarrierRecord()
+            {
+                Value = 0,
+                Child = new IntContract1() { Value = 1 }
+            };
+
+            ITlvContract copy = RoundTrip_Anonymous( carrier );
+
+            var resolvedCopy = copy.Resolve<CarrierRecord>();
+
+            Assert.AreEqual( carrier.Value, resolvedCopy.Value );
+            Assert.AreEqual( ( (IntContract1)carrier.Child ).Value, ( (IntContract1)resolvedCopy.Child ).Value );
+        }
+
+        private static ITlvContract RoundTrip_Anonymous( CarrierRecord carrier )
+        {
+            MemoryStream stream = new MemoryStream();
+            TlvStreamWriter writer = new TlvStreamWriter( stream );
+            TlvStreamReader reader = new TlvStreamReader( stream );
+
+            writer.Write( carrier );
+            stream.Position = 0L;
+            return  reader.ReadContract();
+        }
+    }
+}
