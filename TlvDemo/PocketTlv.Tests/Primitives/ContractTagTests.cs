16199ad260efdd4ab99f47aa8fa6d014f70beb05
antiduh <antiduh@gmail.com>
2020-09-09 16:47:48 +0000

    Added equality test.

--- TlvDemo/PocketTlv.Tests/Primitives/ContractTagTests.cs
+++ TlvDemo/PocketTlv.Tests/Primitives/ContractTagTests.cs
@@ -7,11 +7,20 @@ namespace PocketTlv.Tests.Primitives
     public class ContractTagTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void When_CompositeTagsContainSameData_Equals_ReturnsTrue()
         {
+            var contractTag1 = new ContractTag( 1, new IntTag( 2, 3 ) )
+            {
+                ContractId = 100,
+            };
 
+            var contractTag2 = new ContractTag( 1, new IntTag( 2, 3 ) )
+            {
+                ContractId = 100
+            };
 
-
+            Assert.IsTrue( contractTag1 == contractTag2 );
+            Assert.IsTrue( contractTag1.Equals( contractTag2 ) );
         }
     }
 }
