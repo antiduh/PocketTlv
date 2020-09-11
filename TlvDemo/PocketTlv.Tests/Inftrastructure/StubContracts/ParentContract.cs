f4c37995a164d2b3aa806b2fd998e0c00016032e
antiduh <antiduh@gmail.com>
2020-09-11 21:56:15 +0000

    New contract to show self-resolved subcontracts.

---
+++ TlvDemo/PocketTlv.Tests/Inftrastructure/StubContracts/ParentContract.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace PocketTlv.Tests.Inftrastructure.StubContracts
+{
+    public class ParentContract : ITlvContract
+    {
+        public ParentContract() { }
+
+        public ParentContract( IntContract1 child )
+        {
+            this.Child = child;
+        }
+
+        public IntContract1 Child { get; private set; }
+
+        public bool Equals( ParentContract other )
+        {
+            if( other == null )
+            {
+                return false;
+            }
+
+            return this.Child.Equals( other.Child );
+        }
+
+        public override bool Equals( object other )
+        {
+            return Equals( other as ParentContract );
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Child.GetHashCode();
+        }
+
+        // ----- ITlvContract implementation ------
+
+        int ITlvContract.ContractId => StubTlvs.Parent;
+
+        void ITlvContract.Parse( ITlvParseContext parse )
+        {
+            this.Child = parse.Contract<IntContract1>( 0 );
+        }
+
+        void ITlvContract.Save( ITlvSaveContext save )
+        {
+            save.Contract( 0, this.Child );
+        }
+    }
+}
--- TlvDemo/PocketTlv.Tests/Inftrastructure/StubContracts/StubTlvs.cs
+++ TlvDemo/PocketTlv.Tests/Inftrastructure/StubContracts/StubTlvs.cs
@@ -12,5 +12,6 @@ namespace PocketTlv.Tests.Inftrastructure.StubContracts
         public const int Int1 = Base + 1;
         public const int Int2 = Base + 2;
         public const int Carrier = Base + 3;
+        public const int Parent = Base + 4;
     }
 }
