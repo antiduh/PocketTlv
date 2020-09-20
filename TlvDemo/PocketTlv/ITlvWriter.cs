91f44a323ad5651917c1b2b266f6918cb245358a
antiduh <antiduh@gmail.com>
2020-09-20 03:41:13 +0000

    Comments.

--- TlvDemo/PocketTlv/ITlvWriter.cs
+++ TlvDemo/PocketTlv/ITlvWriter.cs
@@ -2,10 +2,21 @@
 
 namespace PocketTlv
 {
+    /// <summary>
+    /// Specifies the interface to writes TLV tags to a data source, such as a stream.
+    /// </summary>
     public interface ITlvWriter
     {
+        /// <summary>
+        /// Writes a TLV tag to the data source.
+        /// </summary>
+        /// <param name="tag">The tag to write.</param>
         void Write( ITag tag );
 
+        /// <summary>
+        /// Writes a contract to the data source.
+        /// </summary>
+        /// <param name="contract">The contract to write.</param>
         void Write( ITlvContract contract );
     }
 }
